598dbb92bfc73683b99e6ca3c643d823e56ec32c
varmor <morevarun4004@gmail.com>
2023-04-01 20:18:41 +0530

    path modifications

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+# model for peer counsellors data.
+class PeerC(models.Model):
+    first_name = models.CharField(max_length=30)
+    profileP = models.ImageField(default='default.jpg', upload_to='./dashboard/profile_pics')
+    last_name = models.CharField(max_length=30)
+    emailID = models.EmailField()
+    linkedIN = models.CharField(max_length=100)
+    twitter = models.CharField(max_length=100)
+    phoneNO = models.BigIntegerField()
+    about = models.TextField(max_length=500)
+
+    def __str__(self):
+        return f"{self.first_name}'s Profile"
+
--- dashboard/views.py
+++ dashboard/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request, 'dashboard/dashboard.html')
+
+def registrationForm(request):
+    return render(request, 'dashboard/forms.html')
